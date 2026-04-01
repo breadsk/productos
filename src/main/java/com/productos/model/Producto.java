ca9e88a1ca61228f325176f8e9727b4e0d03d588
PN-Docente <pn-docente@duoc.acad>
2026-04-01 16:00:07 -0300

    Traspado a JPA

--- src/main/java/com/productos/model/Producto.java
+++ src/main/java/com/productos/model/Producto.java
@@ -5,33 +5,43 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+//Importaciones de anotaciones de JPA
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+
 //Importamos anotaciones de lombok
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data negera automaticamente
-//getters,setters,toString,equela y hashCode
+
+@Entity
+@Table(name="producto")
 @Data
-
-// @NoArgsConstructor genera el constructor vacio
 @NoArgsConstructor
-
-// @AllArgsConstructor genera el constructor con todos los atributos
 @AllArgsConstructor
 
 public class Producto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotBlank(message = "El nombre del producto no puede estar vacío")
+    @Column(nullable = false)
     private String nombre;
 
     @NotNull(message = "El precio es obligatorio")
     @Positive(message = "El precio debe ser mayor que cero")
+    @Column(nullable = false)
     private Double precio;
 
     @NotNull(message = "El stock es obligatorio")
     @Positive(message = "El stock debe ser mayor que cero")
+    @Column(nullable = false)
     private int stock;
 }
--- src/main/java/com/productos/service/ProductoService.java
+++ src/main/java/com/productos/service/ProductoService.java
@@ -4,6 +4,9 @@ import com.productos.model.Producto;
 import com.productos.repository.ProductoRepository;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 //IMportamos anotaciones de Spring
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +19,23 @@ public class ProductoService {
     private ProductoRepository productoRepository;
 
     public List<Producto> getProducts() {
-        return productoRepository.getAllProducts();
+        return productoRepository.findAll();
     }
 
     public Producto getProductById(int id) {
-        return productoRepository.getProductById(id);
+        Optional<Producto> producto = productoRepository.findById(id);
+        return producto.orElseThrow(() -> new RuntimeException("Paciente no encontrado con ese ID: " + id));
     }
 
     public Producto saveProduct(Producto producto) {
-        return productoRepository.saveProduct(producto);
+        return productoRepository.save(producto);
     }
 
     public Producto updateProduct(Producto producto) {
-        return productoRepository.updateProducto(producto);
+        return productoRepository.save(producto);
     }
 
-    public String deleteProduct(int id) {
-        productoRepository.deleteProducto(id);
-        return "Producto eliminado";
+    public void deleteProduct(int id) {        
+        productoRepository.deleteById(id);
     }
 }
